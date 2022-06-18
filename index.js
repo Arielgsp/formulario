10edf4300f434c0713330a71fd8bd415afe2533c
Ariel <campavelez@gmail.com>
2022-06-18 02:01:57 -0300

    Primer commit proyecto formulario

---
+++ index.js
@@ -0,0 +1,17 @@
+const { log } = require("console");
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = 3000;
+
+
+
+
+app.get(`/`, (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./index.html"));
+})
+
+
+
+app.listen(port, () => console.log(`Servidor ejecutandose en el puerto ${port}`));
